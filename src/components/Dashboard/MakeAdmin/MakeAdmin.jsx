6318610f63826f57558b2392640b7594850bef84
developerabdulmamun <abdulmamun.dev@gmail.com>
2024-06-02 13:31:53 +0600

    Make admin page complete with backend

--- src/components/Dashboard/MakeAdmin/MakeAdmin.jsx
+++ src/components/Dashboard/MakeAdmin/MakeAdmin.jsx
@@ -1,8 +1,26 @@
+"use client";
+
 import CustomButton from "@/components/shared/CustomButton";
+import useAxiosSecure from "@/utils/useAxiosSecure";
 import { Box, TextField, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 const MakeAdmin = () => {
+  const axiosSecure = useAxiosSecure();
+
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const res = await axiosSecure.patch("/make-admin", { email });
+      setMessage(res.data.message);
+    } catch (error) {
+      setMessage(error.response?.data?.message || "An error occurred.");
+    }
+  };
+
   return (
     <>
       <Box bgcolor={"white"} borderRadius={"20px"} p={4}>
@@ -16,16 +34,25 @@ const MakeAdmin = () => {
           gap={1}
           noValidate
           autoComplete="off"
+          onSubmit={handleSubmit}
         >
           <TextField
             placeholder="youremail@gmail.com"
             variant="outlined"
             size="small"
             sx={{ width: { xs: "100%", md: "50%" } }}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
 
-          <CustomButton type="submit">Submit</CustomButton>
+          <CustomButton type="submit">Make Admin</CustomButton>
         </Box>
+
+        {message && (
+          <Typography color={"#f00"} mt={2}>
+            {message}
+          </Typography>
+        )}
       </Box>
     </>
   );
